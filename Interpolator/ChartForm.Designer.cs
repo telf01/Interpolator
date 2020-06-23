5ccdc68790f157f2430bd5f0949a2c101667f3a6
telf01 <twilightsoul.io@gmail.com>
2020-06-23 05:10:59 +0300

    Add chartForm minimum size fix.

--- Interpolator/ChartForm.Designer.cs
+++ Interpolator/ChartForm.Designer.cs
@@ -28,10 +28,10 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
 			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
 			this.label1 = new System.Windows.Forms.Label();
 			this.textBox1 = new System.Windows.Forms.TextBox();
@@ -44,25 +44,25 @@
 			this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			chartArea1.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea1);
-			legend1.Name = "Legend1";
-			this.chart1.Legends.Add(legend1);
+			chartArea4.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea4);
+			legend4.Name = "Legend1";
+			this.chart1.Legends.Add(legend4);
 			this.chart1.Location = new System.Drawing.Point(12, 12);
 			this.chart1.Name = "chart1";
-			series1.ChartArea = "ChartArea1";
-			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-			series1.Legend = "Legend1";
-			series1.LegendText = "Your points";
-			series1.Name = "Series1";
-			series1.YValuesPerPoint = 2;
-			series2.ChartArea = "ChartArea1";
-			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-			series2.Legend = "Legend1";
-			series2.LegendText = "Interpolated Function";
-			series2.Name = "Series2";
-			this.chart1.Series.Add(series1);
-			this.chart1.Series.Add(series2);
+			series7.ChartArea = "ChartArea1";
+			series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+			series7.Legend = "Legend1";
+			series7.LegendText = "Your points";
+			series7.Name = "Series1";
+			series7.YValuesPerPoint = 2;
+			series8.ChartArea = "ChartArea1";
+			series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+			series8.Legend = "Legend1";
+			series8.LegendText = "Interpolated Function";
+			series8.Name = "Series2";
+			this.chart1.Series.Add(series7);
+			this.chart1.Series.Add(series8);
 			this.chart1.Size = new System.Drawing.Size(776, 404);
 			this.chart1.TabIndex = 0;
 			this.chart1.Text = "chart1";
@@ -109,6 +109,7 @@
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.chart1);
+			this.MinimumSize = new System.Drawing.Size(500, 300);
 			this.Name = "ChartForm";
 			this.Text = "ChartForm";
 			this.Load += new System.EventHandler(this.ChartForm_Load);
